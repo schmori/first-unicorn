19d20a763dd60d83a6a3d9f3502d437e512a4ca4
Gloria Geise <glori.geise@gmail.com>
2019-10-13 19:23:07 +0200

    initilal project

---
+++ my-first-one/my-first-one/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  my-first-one
+//
+//  Created by Gloria Geise on 03.10.19.
+//  Copyright © 2019 Gloria Geise. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello World")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
