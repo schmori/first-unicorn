f2a36948035e1657238e9473d0544ec4d4bb9fd4
schmori <56094344+schmori@users.noreply.github.com>
2019-11-18 13:27:05 +0100

    Add files via upload
    landmark test

---
+++ Landmarks/Landmarks/CircleImage.swift
@@ -0,0 +1,24 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Gloria Geise on 17.10.19.
+//  Copyright © 2019 Gloria Geise. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.gray, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
