87f8aeb46ce39dedfa54fe34e823d8b5679ff3c2
Gloria Geise <glori.geise@gmail.com>
2019-11-20 10:37:26 +0100

    added icons, set TabView

--- my-first-one/my-first-one/ContentView.swift
+++ my-first-one/my-first-one/ContentView.swift
@@ -9,9 +9,27 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var selection = 2
+    
+    init() {
+        UITabBar.appearance().backgroundColor = UIColor.orange
+    }
+
     var body: some View {
-        VStack {
-            Text("Hello World")
+        TabView(selection: $selection) {
+            Text("Einstellungen")
+                .tabItem {
+                    Image("settings_icon_32")
+                    Text("Einstellungen")
+                }
+                .tag(1)
+            Text("deine Produktsuche")
+                .tabItem {
+                    Image("supermarket_icon_32")
+                    Text("Suche")
+                }
+                .tag(2)
         }
     }
 }
