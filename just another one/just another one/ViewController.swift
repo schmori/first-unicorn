0e5ff8757fe3a22351cc3c3c4159f24840a275ac
Gloria Geise <glori.geise@gmail.com>
2019-12-29 18:35:49 +0100

    new project

---
+++ just another one/just another one/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  just another one
+//
+//  Created by Gloria Geise on 29.12.19.
+//  Copyright © 2019 Gloria Geise. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
